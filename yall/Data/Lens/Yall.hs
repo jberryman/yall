069efcf07063704461654534afff94e4f5c908dc
Brandon Simmons <brandon.m.simmons@gmail.com>
2012-03-10 15:52:17 -0500

    implemented get/set

--- yall/Data/Lens/Yall.hs
+++ yall/Data/Lens/Yall.hs
@@ -2,26 +2,35 @@
 module Data.Lens.Yall
     where
 
--- from 'categories':
-import Control.Category
-import Control.Categorical.Bifunctor
-
-import Data.Functor.Identity
 import Prelude hiding (id,(.))
 import Control.Category
+
+-- from 'categories':
+import Control.Categorical.Bifunctor
+
 import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Identity
+import Data.Functor.Identity
 
 -- constrain these 'm's to Monad?
 newtype Lens ms mg a b = Lens { runLens :: a -> mg (b -> ms a, b) }
 
+get :: (Monad mg)=> Lens ms mg a b -> a -> mg b
+get (Lens f) = liftM snd . f
+
+set :: (MonadTrans t, Monad (t mg), Monad mg)=> Lens (t mg) mg a b -> b -> a -> t mg a  
+set (Lens f) b = join . liftM (($ b) . fst) . lift . f 
+
 instance (Monad ms, Monad mg)=> Category (Lens ms mg) where
-    id = Lens $ \a -> return (\a'-> return a', a)
+    id = Lens $ return . (,) return
     (Lens f) . (Lens g) = 
         Lens $ \a-> do 
             (bMa,b) <- g a
             (cMb,c) <- f b
             return (cMb >=> bMa, c)
 
+-- BIFUNCTOR: --
 instance (Monad ms, Monad mg)=> PFunctor (,) (Lens ms mg) (Lens ms mg) where
     first = firstDefault
 
@@ -33,20 +42,29 @@ instance (Monad ms, Monad mg)=> Bifunctor (,) (Lens ms mg) (Lens ms mg) (Lens ms
         Lens $ \(a,c)-> do
             (bMa,b) <- f a                       
             (dMc,d) <- g c
-            let setCont (b',d') = do a' <- bMa b'
-                                     c' <- dMc d'
-                                     return (a',c')
+            let setCont (b',d') = liftM2 (,) (bMa b') (dMc d')
             return (setCont, (b,d))
-                          -- in store (\(b',d')->(ba b',dc d')) (b,d)
-    -- bimap (Lens f) (Lens g) = Lens $ \(a,c)-> 
-                         -- let (ba,b) = runStore $ f a                       
-                          --    (dc,d) = runStore $ g c
-                          -- in store (\(b',d')->(ba b',dc d')) (b,d) -- i.e. tuple up
 
 
 -- | a simple lens, suitable for single-constructor types
-type (:->) = Lens Identity Identity
+type (:->) = Lens (IdentityT Identity) Identity
+
+-- | > pureGet l = runIdentity . runIdentity . get l
+pureGet :: (a :-> b) -> a -> b
+pureGet l = runIdentity . get l
+
+-- | > pureSet l b = runIdentity . runIdentityT . set l b
+pureSet :: (a :-> b) -> b -> a -> a
+pureSet l b = runIdentity . runIdentityT . set l b
 
 -- | a lens that can fail in the Maybe monad on the outer type, suitable for a
 -- normal lens on a multi-constructor type
-type (:~>) = Lens Identity Maybe
+type (:~>) = Lens (IdentityT Maybe) Maybe
+
+-- | > maybeGet l = get l
+maybeGet :: (a :~> b) -> a -> Maybe b
+maybeGet l = get l
+
+-- | > maybeSet l b = runIdentityT . set l b
+maybeSet :: (a :~> b) -> b -> a -> Maybe a
+maybeSet l b = runIdentityT . set l b
