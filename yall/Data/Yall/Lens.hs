c4f7e12ae636e95a58988c6c027fd6af67cc6ef7
Brandon Simmons <brandon.m.simmons@gmail.com>
2012-03-30 20:00:40 -0400

    using m/w naming convention for setter/getter monad envs

--- yall/Data/Yall/Lens.hs
+++ yall/Data/Yall/Lens.hs
@@ -56,13 +56,12 @@ import Data.Functor.Identity
 
 {-
  TODO: explore monadic lenses (is the concept sound)
-            alter set monad to w, get monad to m
        initial release
        template haskell library
 -}
 
 -- constrain these 'm's to Monad?
-newtype Lens ms mg a b = Lens { runLens :: a -> mg (b -> ms a, b) }
+newtype Lens w m a b = Lens { runLens :: a -> m (b -> w a, b) }
 
 -- | A lens in which the setter returns its result in the trivial identity 
 -- monad. This is appropriate e.g. for traditional partial lenses, where there is
@@ -76,16 +75,16 @@ lensM g = lensMW g . fmap (liftM $ fmap return)
 -- | get, returning the result in a Monadic environment. This is appropriate
 -- e.g. for traditional partial lenses on multi-constructor types. See also
 -- 'setM'
-getM :: (Monad mg)=> Lens ms mg a b -> a -> mg b
+getM :: (Monad m)=> Lens w m a b -> a -> m b
 getM (Lens f) = liftM snd . f
 
 -- | set, returning the result in the getter\'s Monadic environment, running 
 -- the setter\'s trivial Identity monad. 
-setM :: (Monad mg)=> LensM mg a b -> b -> a -> mg a
+setM :: (Monad m)=> LensM m a b -> b -> a -> m a
 setM (Lens f) b = liftM (runIdentity . ($ b) . fst) . f
 
 -- | modify the inner value within the getter\'s Monadic environment 
-modifyM :: (Monad mg)=> LensM mg a b -> (b -> b) -> a -> mg a
+modifyM :: (Monad m)=> LensM m a b -> (b -> b) -> a -> m a
 modifyM = undefined
 
 -- | Create a monadic Lens from a setter and getter.
@@ -97,12 +96,12 @@ lensMW g s = Lens $ \a-> liftM2 (,) (s a) (g a)
 
 -- | set, 'lift'ing the outer (getter\'s) Monadic environment to the type of
 -- the setter monad transformer.
-setLiftM :: (Monad (t mg), MonadTrans t, Monad mg)=> Lens (t mg) mg a b -> b -> a -> t mg a  
+setLiftM :: (Monad (t m), MonadTrans t, Monad m)=> Lens (t m) m a b -> b -> a -> t m a  
 setLiftM (Lens f) b = join . liftM (($ b) . fst) . lift . f 
 
 -- | set, like 'setLiftM' but we 'lift' the /inner/ setter\'s environment to
 -- the outer getter monad transformer.
-setLiftW :: (MonadTrans t, Monad (t ms), Monad ms)=> Lens ms (t ms) a b -> b -> a -> t ms a
+setLiftW :: (MonadTrans t, Monad (t w), Monad w)=> Lens w (t w) a b -> b -> a -> t w a
 setLiftW (Lens f) b = undefined
 
 -- | set, combining the effects of the identical setter and getter Monads with
@@ -110,7 +109,7 @@ setLiftW (Lens f) b = undefined
 setJoin :: (Monad m)=> Lens m m a b -> b -> a -> m a
 setJoin = undefined
 
-instance (Monad ms, Monad mg)=> Category (Lens ms mg) where
+instance (Monad w, Monad m)=> Category (Lens w m) where
     id = Lens $ return . (,) return
     (Lens f) . (Lens g) = 
         Lens $ \a-> do 
@@ -119,14 +118,14 @@ instance (Monad ms, Monad mg)=> Category (Lens ms mg) where
             return (cMb >=> bMa, c)
 
 -- BIFUNCTOR: --
-instance (Monad ms, Monad mg)=> PFunctor (,) (Lens ms mg) (Lens ms mg) where
+instance (Monad w, Monad m)=> PFunctor (,) (Lens w m) (Lens w m) where
   --first :: Lens a b -> Lens (a,x) (b,x)
     first = firstDefault
 
-instance (Monad ms, Monad mg)=> QFunctor (,) (Lens ms mg) (Lens ms mg) where
+instance (Monad w, Monad m)=> QFunctor (,) (Lens w m) (Lens w m) where
     second = secondDefault
 
-instance (Monad ms, Monad mg)=> Bifunctor (,) (Lens ms mg) (Lens ms mg) (Lens ms mg) where
+instance (Monad w, Monad m)=> Bifunctor (,) (Lens w m) (Lens w m) (Lens w m) where
     bimap (Lens f) (Lens g) = 
         Lens $ \(a,c)-> do
             (bMa,b) <- f a                       
@@ -134,21 +133,21 @@ instance (Monad ms, Monad mg)=> Bifunctor (,) (Lens ms mg) (Lens ms mg) (Lens ms
             let setCont (b',d') = liftM2 (,) (bMa b') (dMc d')
             return (setCont, (b,d))
 
-instance (Monad ms, Monad mg)=> C.Functor ((,) x) (Lens ms mg) (Lens ms mg) where
+instance (Monad w, Monad m)=> C.Functor ((,) x) (Lens w m) (Lens w m) where
     fmap = second
 
 {-
 TODO: EXPLORE THIS
-instance (Monad ms, Monad mg)=> C.Functor ((->) r) (Lens ms mg) (Lens ms mg) where
-instance (Monad ms, Monad mg)=> C.Functor IO (Lens ms mg) (Lens ms mg) where
+instance (Monad w, Monad m)=> C.Functor ((->) r) (Lens w m) (Lens w m) where
+instance (Monad w, Monad m)=> C.Functor IO (Lens w m) (Lens w m) where
     fmap (Lens f) = Lens $ \ioa-> do
         (bMa,b) <- liftIO f ioa
         return (??, return b)
 
--- It seems sum and recursive types will have issues with Functor, which we
+-- It seew sum and recursive types will have issues with Functor, which we
 -- wouldn't have for a type like Iso. Perhaps we should include such a type
 -- instead of the straight iso function in data-lens, e.g.:
---     data Iso mg ms a b = Iso (a -> mg b) (b -> ms a)
+--     data Iso m w a b = Iso (a -> m b) (b -> w a)
 --     isoL :: Iso a b -> (a :-> b)         -- this NEEDS to be polymorphic in monad, for composability
 --     instance C.Functor (Either x) Iso Iso
 --     instance C.Functor [] Iso Iso    ... etc.
@@ -156,11 +155,11 @@ instance (Monad ms, Monad mg)=> C.Functor IO (Lens ms mg) (Lens ms mg) where
 -- Also take a look at notes. Many of those instances could potentially work
 -- for an Iso type.
 
-instance (Monad ms, Monad mg)=> C.Functor [] (Lens ms mg) (Lens ms mg) where
+instance (Monad w, Monad m)=> C.Functor [] (Lens w m) (Lens w m) where
     fmap (Lens f) = Lens $ \as -> do
         bMabs <- mapM f as
         return (error "violates put-get if we just do a zip" , map snd bMabs)
-instance (Monad ms, Monad mg)=> C.Functor (Either x) (Lens ms mg) (Lens ms mg) where
+instance (Monad w, Monad m)=> C.Functor (Either x) (Lens w m) (Lens w m) where
 -}
 
 
@@ -175,41 +174,41 @@ instance HasTerminalObject Lens where
     terminate = Lens $ \a-> (\()-> a, ())  -- this DOES obey the lens laws
 -}
 
-instance (Monad ms, Monad mg)=> Associative (Lens ms mg) (,) where
+instance (Monad w, Monad m)=> Associative (Lens w m) (,) where
   --associate :: Lens ((a,b),c) (a,(b,c))
     associate = Lens $ \((a,b),c)-> return (\(a',(b',c'))-> return ((a',b'),c'), (a,(b,c)))
 
-instance (Monad ms, Monad mg)=> Disassociative (Lens ms mg) (,) where
+instance (Monad w, Monad m)=> Disassociative (Lens w m) (,) where
   --disassociate :: Lens (a,(b,c)) ((a,b),c)
     disassociate =Lens $ \(a,(b,c))-> return (\((a',b'),c') -> return (a',(b',c')), ((a,b),c))
 
-instance (Monad ms, Monad mg)=> Braided (Lens ms mg) (,) where
+instance (Monad w, Monad m)=> Braided (Lens w m) (,) where
   --braid :: Lens (a,b) (b,a)
     braid = Lens $ \(a,b) -> return (\(b',a')-> return (a',b') , (b,a))
 
-instance (Monad ms, Monad mg)=> Symmetric (Lens ms mg) (,)
+instance (Monad w, Monad m)=> Symmetric (Lens w m) (,)
 
 -- (CO)MONOIDAL ----------------------------------------
 
-type instance Id (Lens ms mg) (,) = ()
+type instance Id (Lens w m) (,) = ()
 
 -- THIS ABSTRACTS THE dropl/r FUNCTIONS FROM GArrow:
-instance (Monad ms, Monad mg)=> Monoidal (Lens ms mg) (,) where  
+instance (Monad w, Monad m)=> Monoidal (Lens w m) (,) where  
   --idl :: Lens ((), a)  a
     idl = Lens $ \((),a)-> return (\a'-> return ((),a'), a)
     idr = Lens $ \(a,())-> return (\a'-> return (a',()), a)
 
-instance (Monad ms, Monad mg)=> Comonoidal (Lens ms mg) (,) where
+instance (Monad w, Monad m)=> Comonoidal (Lens w m) (,) where
   --coidl :: Lens a ((),a)
     coidl = Lens $ \a-> return (\((),a')-> return a', ((),a))
     coidr = Lens $ \a-> return (\(a',())-> return a', (a,()))
 
 -- combinators from PreCartesian that preserve strict well-behavedness
 
-fstL :: (Monad mg, Monad ms)=> Lens ms mg (a,b) a
+fstL :: (Monad m, Monad w)=> Lens w m (a,b) a
 fstL = Lens $ \(a,b)-> return (\a'-> return (a',b) , a)
 
-sndL :: (Monad mg, Monad ms)=> Lens ms mg (a,b) b
+sndL :: (Monad m, Monad w)=> Lens w m (a,b) b
 sndL = Lens $ \(a,b)-> return (\b'-> return (a,b') , b)
 
 
@@ -218,12 +217,12 @@ sndL = Lens $ \(a,b)-> return (\b'-> return (a,b') , b)
 -- | 
 --
 -- > eitherL = id ||| id
-eitherL :: (Monad mg, Monad ms)=> Lens ms mg (Either a a) a
+eitherL :: (Monad m, Monad w)=> Lens w m (Either a a) a
 eitherL = id ||| id -- (codiag) DEFAULT
 
 
 -- TODO: Iso will be a proper PreCrtesian, so maybe we don't want to steal this name:
-(|||) :: (Monad mg, Monad ms)=> Lens ms mg a c -> Lens ms mg b c -> Lens ms mg (Either a b) c
+(|||) :: (Monad m, Monad w)=> Lens w m a c -> Lens w m b c -> Lens w m (Either a b) c
 Lens f ||| Lens g = Lens $ either (handleL . f) (handleR . g) 
     where handleL = liftM $ first (liftM Left .)
           handleR = liftM $ first (liftM Right .)
@@ -236,13 +235,13 @@ Lens f ||| Lens g = Lens $ either (handleL . f) (handleR . g)
 --"factor . distribute" factor . distribute = id
 --"distribute . factor" distribute . factor = id
 
-factorL :: (Monad mg, Monad ms)=>Lens ms mg (Either (a,b) (a,c)) (a, Either b c)
+factorL :: (Monad m, Monad w)=>Lens w m (Either (a,b) (a,c)) (a, Either b c)
 factorL = Lens $ either (\(a,b)-> return (s ,(a, Left b))) (\(a,c)-> return (s, (a, Right c)))
     where s (a, ebc) = return $ either (Left . (,) a) (Right . (,) a) ebc
 --factor = second inl ||| second inr -- DEFAULT
 
 
-distributeL :: (Monad mg, Monad ms)=>Lens ms mg (a, Either b c) (Either (a,b) (a,c))
+distributeL :: (Monad m, Monad w)=>Lens w m (a, Either b c) (Either (a,b) (a,c))
 distributeL = Lens $ \(a,ebc)-> 
     return (return . factor, bimap ((,) a) ((,) a) ebc)
 
